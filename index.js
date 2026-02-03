a9beebf445f4ff0a8768fabeda32d1bb881755c9
Jackson <Jacksonwgray14@gmail.com>
2026-02-03 14:44:45 -0700

    Page created, basic functionality

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+let add = document.getElementById("add");
+let text = document.getElementById("RM").value;
+let remove = document.getElementById("remove");
+let tasks = [];
+let li = "<li>" + text + "</li>";
+let ll = document.getElementById("Listitems").childNodes;
+let ul = document.getElementById("Listitems");
 
-document.getElementById("add").onclick = function() {
+add.onclick = function() {
   var text = document.getElementById("RM").value; 
   var li = "<li>" + text + "</li>";
-  document.getElementById("Listitems").innerHTML += li;
+  tasks.push(li);
+  document.getElementById("Listitems").innerHTML += tasks;
   document.getElementById("RM").value = ""; // clear the value
   event.preventDefault(); //prevent refresh
 }
-
+ll.onclick = function(){
+    ll.style.textDecoration = "line-through";
+    event.preventDefault();
+}
